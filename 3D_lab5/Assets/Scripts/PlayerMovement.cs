2d1cab7bd308f18df8b7ffecd7a70f77e03f738a
Yurii-Polischukkk <113755012+Yurii-Polischukkk@users.noreply.github.com>
2022-11-12 03:08:03 +0200

    sound

---
+++ 3D_lab5/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+
+
+    private Animator animator;
+    private int currentAnimation;
+    private List<string> animations;
+    private Vector3 moveDirection;
+    private Vector3 velocity;
+
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float gravity;
+
+    [SerializeField] private float jumpHeight;
+
+    private CharacterController controller;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        animations = new List<string>()
+            {
+                "Hit1",
+                "Fall1",
+                "Attack1h1",
+            };
+    }
+
+    private void Update()
+    {
+        Move();
+        if (Input.GetMouseButtonDown(1))
+        {
+            animator.SetTrigger("Attack1h1");
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            animator.SetTrigger("Hit1");
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            animator.SetTrigger("Fall1");
+        }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            animator.SetTrigger("Up");
+        }
+    }
+
+    private void Move()
+    {
+        isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        float moveZ = Input.GetAxis("Vertical");
+
+        moveDirection = new Vector3(0, 0, moveZ);
+        moveDirection = transform.TransformDirection(moveDirection);
+
+        if(isGrounded)
+        {
+            if (moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
+            {
+                Walk();
+            }
+            else if (moveDirection != Vector3.zero && Input.GetKey(KeyCode.RightShift))
+            {
+                Run();
+            }
+            else if (moveDirection == Vector3.zero)
+            {
+                Idle();
+            }
+
+            moveDirection *= moveSpeed;
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+            }
+        }       
+
+        controller.Move(moveDirection * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void Idle()
+    {
+
+    }
+    private void Walk()
+    {
+        moveSpeed = walkSpeed;
+    }
+    private void Run()
+    {
+        moveSpeed = runSpeed;
+    }
+    private void Jump()
+    {
+        velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+
+    }
+
+    void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
